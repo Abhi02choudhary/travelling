6ed46afc5b57ef1e76392ae4dee0e70034e7c552
Abhi02choudhary <161602624+Abhi02choudhary@users.noreply.github.com>
2024-03-25 14:30:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom"; // Correct import statement
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Correct usage of ReactDOM
+root.render(<App />);
